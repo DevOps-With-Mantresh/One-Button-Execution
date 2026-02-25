d9b6f7d6f24664b5ef7dd96fd100c107436db71c
Mantresh Kumar <kmantresh@gmail.com>
2026-02-26 03:43:12 +0530

    public access blocked

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,13 +3,13 @@ module "eks" {
   version = "19.21.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.35"
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access  = false
 
   enable_irsa = true
 
