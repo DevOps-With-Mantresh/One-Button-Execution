8720b6c4e74fcb66a490f3c0f2f1112489cca93f
Mantresh Kumar <kmantresh@gmail.com>
2026-02-28 20:08:49 +0530

    added 1 node

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -32,7 +32,7 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       desired_size = 1
-      min_size     = 1
+      min_size     = 0 
       max_size     = 2
 
       instance_types = ["t3.medium"]
